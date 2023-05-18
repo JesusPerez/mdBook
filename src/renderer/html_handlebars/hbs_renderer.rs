66d27e3175bffef9f791d753036407c761d6ec04
Jesús Pérez <jpl@jesusperez.pro>
2023-05-18 14:15:54 +0100

    add absolute link to index pages

--- src/renderer/html_handlebars/hbs_renderer.rs
+++ src/renderer/html_handlebars/hbs_renderer.rs
@@ -131,7 +131,7 @@ impl HtmlHandlebars {
 
         if ctx.is_index {
             ctx.data.insert("path".to_owned(), json!("index.md"));
-            ctx.data.insert("path_to_root".to_owned(), json!(""));
+            // ctx.data.insert("path_to_root".to_owned(), json!(""));
             ctx.data.insert("is_index".to_owned(), json!(true));
             let rendered_index = ctx.handlebars.render("index", &ctx.data)?;
             let rendered_index =
